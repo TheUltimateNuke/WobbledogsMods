82a2addcde39021a859d6415c0e26ae87c3b5fae
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2023-11-25 10:48:23 -0500

    added TutorialSkip

---
+++ TutorialSkip/Mod.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System.Threading.Tasks;
+//using WobbleOn.Debugging;
+
+namespace TutorialSkip
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Mod : BaseUnityPlugin
+    {
+        public static ConfigFile ModConfig { get; private set; }
+
+        //public static ExposeClassFieldsToConfig exposed_CheatEngine;
+
+        private void Awake()
+        {
+            ModConfig = Config;
+
+            //exposed_CheatEngine = new ExposeClassFieldsToConfig(CheatEngine.cheatRef);
+            Logger.LogInfo($"Mod {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void UpdateTutorialSkip()
+        {
+            if (CheatEngine.cheatRef == null) return;
+            CheatEngine.cheatRef.tutorialEnabled = false;
+            if (TutorialController.IsTutorialActive())
+                TutorialController.SetCurrentState(TutorialState.FINAL_UNLOCKS);
+        }
+
+        private void Update()
+        {
+            UpdateTutorialSkip();
+        }
+    }
+}
